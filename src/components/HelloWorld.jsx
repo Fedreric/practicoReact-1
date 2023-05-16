70c31bfd30bf20c516c94f2652a76ed9a41d62f4
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-05-16 16:15:59 -0300

    Se realiza el ejercicio 1

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorld = () => {
+    return (
+        <section>
+            <h1>Hello World!</h1>
+        </section>
+    );
+};
+
+export default HelloWorld;
